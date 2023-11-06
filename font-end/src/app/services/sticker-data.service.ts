90821fddc7809f687a735d0789ea3defa3c5f9bb
Imweeraya <weerayakosaldilok@gmail.com>
2023-11-07 00:53:20 +0700

    add reward icon

--- font-end/src/app/services/sticker-data.service.ts
+++ font-end/src/app/services/sticker-data.service.ts
@@ -66,7 +66,6 @@ export class StickerDataService {
       '../../../assets/img/ActivityIcon/laundry2.png',
       '../../../assets/img/ActivityIcon/triangle2.png',
       '../../../assets/img/ActivityIcon/vegetables.png',
-
     ];
     //--------------------------- Praise----------------------------
     this.praiseTheme = [
@@ -95,11 +94,9 @@ export class StickerDataService {
 
     //--------------------------- Point----------------------------
     this.pointBg = [
-      
       '../../../assets/img/PointSticker/Bg/Bgpoint6.png',
-       '../../../assets/img/PointSticker/Bg/Bgpoint1.png',
+      '../../../assets/img/PointSticker/Bg/Bgpoint1.png',
       '../../../assets/img/PointSticker/Bg/Bgpoint7.png',
-      
 
       '../../../assets/img/PointSticker/Bg/Bgpoint8.png',
       '../../../assets/img/PointSticker/Bg/Bgpoint10.png',
@@ -120,15 +117,15 @@ export class StickerDataService {
 
       '../../../assets/img/PointSticker/Bg/Bgpoint4.png',
       '../../../assets/img/PointSticker/Bg/Bgpoint19.png',
-      // '../../../assets/img/PointSticker/Bg/Bgpoint20.png',
+      '../../../assets/img/PointSticker/Bg/Bgpoint20.png',
 
       '../../../assets/img/PointSticker/Bg/Bgpoint21.png',
       '../../../assets/img/PointSticker/Bg/Bgpoint22.png',
       '../../../assets/img/PointSticker/Bg/Bgpoint23.png',
       '../../../assets/img/PointSticker/Bg/Bgpoint24.png',
-      
+
       '../../../assets/img/PointSticker/Bg/Bgpoint25.png',
-      
+
       '../../../assets/img/PointSticker/Bg/Bgpoint27.png',
       '../../../assets/img/PointSticker/Bg/Bgpoint28.png',
       '../../../assets/img/PointSticker/Bg/Bgpoint29.png',
@@ -178,62 +175,65 @@ export class StickerDataService {
     ];
 
     this.rewardIcon = [
-      '../../../assets/img/RewardIcon/book.png',
-      '../../../assets/img/RewardIcon/cotton-candy.png',
-      '../../../assets/img/RewardIcon/ice-cream1.png',
-      '../../../assets/img/RewardIcon/pencil-case.png',
-      '../../../assets/img/RewardIcon/storage-box.png',
       '../../../assets/img/RewardIcon/teddy-bear.png',
-
-      '../../../assets/img/RewardIcon/block.png',
-      '../../../assets/img/RewardIcon/board-game.png',
-      '../../../assets/img/RewardIcon/book-stack.png',
-      '../../../assets/img/RewardIcon/books.png',
-      '../../../assets/img/RewardIcon/cake.png',
-      '../../../assets/img/RewardIcon/camping-tent.png',
-      '../../../assets/img/RewardIcon/card-games.png',
-      '../../../assets/img/RewardIcon/chocolate.png',
-      '../../../assets/img/RewardIcon/cocktail.png',
-      '../../../assets/img/RewardIcon/colored-pencils.png',
-      '../../../assets/img/RewardIcon/cookies.png',
-      '../../../assets/img/RewardIcon/cupcake.png',
+      '../../../assets/img/RewardIcon/storage-box.png',
+      '../../../assets/img/RewardIcon/donation.png',
       '../../../assets/img/RewardIcon/dino.png',
-      '../../../assets/img/RewardIcon/dog.png',
+      '../../../assets/img/RewardIcon/board-game.png',
+      '../../../assets/img/RewardIcon/card-games.png',
+      '../../../assets/img/RewardIcon/block.png',
+      '../../../assets/img/RewardIcon/mystery.png',
+      '../../../assets/img/RewardIcon/camping-tent.png',
       '../../../assets/img/RewardIcon/doll.png',
-      '../../../assets/img/RewardIcon/dress.png',
-      '../../../assets/img/RewardIcon/dress1.png',
-      '../../../assets/img/RewardIcon/fast-food.png',
-      '../../../assets/img/RewardIcon/fruits.png',
+      '../../../assets/img/RewardIcon/doll-house.png',
       '../../../assets/img/RewardIcon/giftbox.png',
       '../../../assets/img/RewardIcon/happy.png',
+      '../../../assets/img/RewardIcon/stickers.png',
+      '../../../assets/img/RewardIcon/rubik.png',
+      '../../../assets/img/RewardIcon/toys1.png',
+      '../../../assets/img/RewardIcon/toys.png',
+      '../../../assets/img/RewardIcon/book.png',
+      '../../../assets/img/RewardIcon/book-stack.png',
+      '../../../assets/img/RewardIcon/books.png',
+      '../../../assets/img/RewardIcon/pencil-case.png',
+      '../../../assets/img/RewardIcon/colored-pencils.png',
+      '../../../assets/img/RewardIcon/pencil.png',
+      '../../../assets/img/RewardIcon/school-bag.png',
+      '../../../assets/img/RewardIcon/school-bag1.png',
+
+      '../../../assets/img/RewardIcon/cotton-candy.png',
+      '../../../assets/img/RewardIcon/ice-cream1.png',
+      '../../../assets/img/RewardIcon/cake.png',
+      '../../../assets/img/RewardIcon/cookies.png',
+      '../../../assets/img/RewardIcon/cupcake.png',
+      '../../../assets/img/RewardIcon/chocolate.png',
+      '../../../assets/img/RewardIcon/cocktail.png',
+      '../../../assets/img/RewardIcon/fast-food.png',
+      '../../../assets/img/RewardIcon/fruits.png',
       '../../../assets/img/RewardIcon/ice-cream.png',
       '../../../assets/img/RewardIcon/ikura.png',
       '../../../assets/img/RewardIcon/milk.png',
-      '../../../assets/img/RewardIcon/mystery.png',
-      '../../../assets/img/RewardIcon/neck-pillow.png',
-      '../../../assets/img/RewardIcon/pencil.png',
-      '../../../assets/img/RewardIcon/piggy-bank.png',
-      '../../../assets/img/RewardIcon/pillow1.png',
-      '../../../assets/img/RewardIcon/pillows.png',
-      '../../../assets/img/RewardIcon/rubik.png',
-      '../../../assets/img/RewardIcon/school-bag.png',
-      '../../../assets/img/RewardIcon/stickers.png',
       '../../../assets/img/RewardIcon/strawberry-cake.png',
       '../../../assets/img/RewardIcon/sushi.png',
       '../../../assets/img/RewardIcon/sweets.png',
+
+      '../../../assets/img/RewardIcon/pillow1.png',
+      '../../../assets/img/RewardIcon/pillows.png',
+      '../../../assets/img/RewardIcon/neck-pillow.png',
+      '../../../assets/img/RewardIcon/dress.png',
+      '../../../assets/img/RewardIcon/dress1.png',
       '../../../assets/img/RewardIcon/t-shirt.png',
       '../../../assets/img/RewardIcon/tshirt.png',
-      '../../../assets/img/RewardIcon/toys1.png',
-      '../../../assets/img/RewardIcon/toys.png',
-      
+
+      '../../../assets/img/RewardIcon/dog.png',
+
+      '../../../assets/img/RewardIcon/piggy-bank.png',
+      '../../../assets/img/RewardIcon/coin.png',
     ];
-
-
   }
 
   activityIcon: any[];
   activityTheme: any[];
-
 
   selectedThemeActivity = -1;
 
@@ -242,8 +242,10 @@ export class StickerDataService {
     console.log('set Theme complete!');
   }
 
-  getActivityTheme(){
-    const themeA = {activityTheme : this.activityTheme[this.selectedThemeActivity]};
+  getActivityTheme() {
+    const themeA = {
+      activityTheme: this.activityTheme[this.selectedThemeActivity],
+    };
     return themeA;
   }
 
@@ -255,29 +257,27 @@ export class StickerDataService {
     return this.activityIcon;
   }
 
-    // ---------------------------------- Praise -----------------------------------------
-    praiseTheme: any[];
+  // ---------------------------------- Praise -----------------------------------------
+  praiseTheme: any[];
 
+  selectedThemepraise = -1;
 
-    selectedThemepraise = -1;
-  
-    setpraiseSticker(select: number) {
-      this.selectedThemepraise = select;
-      console.log('set Theme complete!');
-    }
-  
-    getpraiseTheme(){
-      const themeA = {praiseTheme : this.praiseTheme[this.selectedThemepraise]};
-      return themeA;
-    }
-  
-    getAllpraiseTheme() {
-      return this.praiseTheme;
-    }
+  setpraiseSticker(select: number) {
+    this.selectedThemepraise = select;
+    console.log('set Theme complete!');
+  }
+
+  getpraiseTheme() {
+    const themeA = { praiseTheme: this.praiseTheme[this.selectedThemepraise] };
+    return themeA;
+  }
+
+  getAllpraiseTheme() {
+    return this.praiseTheme;
+  }
 
   // ---------------------------------- Feeling -----------------------------------------
   feelingTheme: any[];
-
 
   selectedThemefeeling = -1;
 
@@ -286,8 +286,10 @@ export class StickerDataService {
     console.log('set Theme complete!');
   }
 
-  getfeelingTheme(){
-    const themeA = {feelingTheme : this.feelingTheme[this.selectedThemefeeling]};
+  getfeelingTheme() {
+    const themeA = {
+      feelingTheme: this.feelingTheme[this.selectedThemefeeling],
+    };
     return themeA;
   }
 
@@ -311,7 +313,6 @@ export class StickerDataService {
   rewardIcon: any[];
   rewardTheme: any[];
 
-
   selectedThemereward = -1;
 
   setrewardSticker(select: number) {
@@ -319,8 +320,8 @@ export class StickerDataService {
     console.log('set Theme complete!');
   }
 
-  getrewardTheme(){
-    const themeA = {rewardTheme : this.rewardTheme[this.selectedThemereward]};
+  getrewardTheme() {
+    const themeA = { rewardTheme: this.rewardTheme[this.selectedThemereward] };
     return themeA;
   }
 
@@ -334,11 +335,11 @@ export class StickerDataService {
 
   // -------------------------- history ------------------------------
   historyIndex = 0;
-  setHistoryIndex(index:number){
+  setHistoryIndex(index: number) {
     this.historyIndex = index;
   }
 
-  getHistoryIndex(){
+  getHistoryIndex() {
     return this.historyIndex;
   }
 
@@ -346,14 +347,11 @@ export class StickerDataService {
 
   showTutorial = false;
 
-  getshowtutorial(){
-    return this.showTutorial
+  getshowtutorial() {
+    return this.showTutorial;
   }
 
-  setshowtutorial(show:boolean){
+  setshowtutorial(show: boolean) {
     this.showTutorial = show;
   }
-
-  
-
 }
